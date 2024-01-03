2e4ef4b55ddc57913cec29c500fdd296960a24a7
songjc <969378911@qq.com>
2024-01-03 10:56:43 +0800

    增加服务端口列表有关功能

--- sdks/scheduler/jobset.go
+++ sdks/scheduler/jobset.go
@@ -86,3 +86,41 @@ func (c *Client) JobSetLocalFileUploaded(req JobSetLocalFileUploadedReq) error {
 
 	return fmt.Errorf("unknow response content type: %s", contType)
 }
+
+type JobSetGetServiceListReq struct {
+	JobSetID
+}
+
+type JobSetGetServiceListResp struct {
+	ServiceList []JobSetServiceInfo `json:"serviceList"`
+}
+
+func (c *Client) JobSetGetServiceList(req JobSetGetServiceListReq) (*JobSetGetServiceListResp, error) {
+	url, err := url.JoinPath(c.baseURL, "/jobSet/getServiceList")
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := myhttp.GetJSON(url, myhttp.RequestParam{
+		Body: req,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	contType := resp.Header.Get("Content-Type")
+	if strings.Contains(contType, myhttp.ContentTypeJSON) {
+		var codeResp response[JobSetGetServiceListResp]
+		if err := serder.JSONToObjectStream(resp.Body, &codeResp); err != nil {
+			return nil, fmt.Errorf("parsing response: %w", err)
+		}
+
+		if codeResp.Code == errorcode.OK {
+			return &codeResp.Data, nil
+		}
+
+		return nil, codeResp.ToError()
+	}
+
+	return nil, fmt.Errorf("unknow response content type: %s", contType)
+}
