31f0651051ca057f37b3bc29bec2c19549bb1df6
Sydonian <794346190@qq.com>
2024-05-19 16:36:09 +0800

    修复调试错误

--- sdks/imfs/models.go
+++ sdks/imfs/models.go
@@ -3,12 +3,9 @@ package imsdk
 import schsdk "gitlink.org.cn/cloudream/common/sdks/scheduler"
 
 const (
-	EnvPackageList    = "IMFS_PACKAGE_LIST"
-	EnvServiceAddress = "IMFS_SERVICE_ADDRESS"
-	EnvLocalJobID     = "IMFS_LOCAL_JOB_ID"
-	EnvJobsetID       = "IMFS_JOBSET_ID"
-	EnvListeningList  = "IMFS_PROXY_LSTENING_LIST"
-	EnvServingList    = "IMFS_PROXY_SERVING_LIST"
+	EnvPackageList     = "IMFS_PACKAGE_LIST"
+	EnvServiceAddress  = "IMFS_SERVICE_ADDRESS"
+	EnvPCMClientConfig = "IMFS_PCM_CLIENT_CONFIG"
 )
 
 //代表本任务需要访问的服务
